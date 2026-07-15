54a01a662e108f46fb76c77caddecd032372b934
Jason Abbott <woebane@gmail.com>
2026-07-14 17:02:52 -0700

    Added main state

---
+++ Scripts/States/main_menu.gd
@@ -0,0 +1,17 @@
+class_name MainMenu extends StateMachineState
+
+
+func _on_play_button_button_up() -> void:
+	our_state_machine.switch_state("Play")
+
+
+func _on_upgrade_button_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_button_button_up() -> void:
+	our_state_machine.switch_state("Credits")
+
+
+func _on_exit_button_button_up() -> void:
+	get_tree().quit()
